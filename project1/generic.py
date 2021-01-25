3237a7133d1f8c7a4a14b5e4e29fa8ba04742332
Anthony Hallak <ahall012@ucr.edu>
2021-01-24 22:48:49 -0800

    Added skeleton generic.py and main.py

---
+++ project1/generic.py
@@ -0,0 +1,48 @@
+# this implements the generic search algorithm and takes in a queueing 
+# function and an inital problem state for the 8 puzzle
+
+class Problem:
+    def __init__(self, arr, type=8, goal_state):
+        # initial state is the given state from the user
+        self.initial_state = arr
+        # self.operators = ['left','down','right','up']
+        # we turned the above line into tuples to add to traverse the grid
+        self.operators = [(-1,0),(0,-1),(1,0),(0,1)]
+        if type == 8:
+            self.goal_state = [['1','2','3'],['4','5','6'],['7','8','0']]
+        else:
+            self.goal_state = goal_state
+
+class Node:
+    def __init__(self, state):
+        self.state = state
+
+def make_node(given_state):
+    # TODO
+
+def make_queue(node_arr):
+    # TODO
+
+def queueing_function(nodes, expansions):
+    # TODO
+
+def expand(curr_node, operators):
+    # TODO 
+
+def generic(problem: Problem, queueing_function):
+    # we need to make a list of nodes that have an order to visit
+    nodes = make_queue(make_node(problem.initial_state))
+    while True:
+        if len(nodes) == 0:
+            return "No solution"
+        # pop off the first node of the queue
+        curr_node = nodes[0]
+        # remove the first node
+        nodes = nodes[1:]
+        # if problem.goal_state(node.state):
+        if curr_node in problem.goal_state:
+            return curr_node
+        # the node is not the goal state
+        # so we expand our nodes and restart
+        nodes = queueing_function(nodes, expand(curr_node, problem.operators))
+    return "No solution"
---
+++ project1/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+'''
+ This is the main driver code for project 1
+ This is where the user gives input
+ This is then passed to generic.py 
+ and we get back our solution (if there is one)
+'''
+import random
+
+def main():
+    print("Welcome to Anthony Hallak's 8-puzzle solver!\n")
+    try:
+        puzzle_type = int(input("Type \"1\" to use a default random puzzle or \"2\" to enter your own\n"))
+    except:
+        print("Sorry, we could not understand that :(")
+        return 1
+
+    if puzzle_type == 1:
+        random_arr = [['1','2','3'], ['4','5','6'],['7','8','0']]
+        random.shuffle(random_arr[0])
+        random.shuffle(random_arr[1])
+        random.shuffle(random_arr[2])
+        random.shuffle(random_arr)
+        print(random_arr)
+    elif puzzle_type == 2:
+        print("Enter your puzzle, use a zero to represent the blank")
+        row_1 = str(input("Enter the first row, use spaces or tabs between numbers\n"))
+        row_1 = row_1.split()
+        if len(row_1) < 3 or len(row_1) > 3:
+            print("That doesn't look like 3 numbers, please try again later")
+            return 1
+        row_2 = str(input("Enter the first row, use spaces or tabs between numbers\n"))
+        row_2 = row_2.split()
+        if len(row_2) < 3 or len(row_2) > 3:
+            print("That doesn't look like 3 numbers, please try again later")
+            return 1
+        row_3 = str(input("Enter the first row, use spaces or tabs between numbers\n"))
+        row_3 = row_3.split()
+        if len(row_3) < 3 or len(row_3) > 3:
+            print("That doesn't look like 3 numbers, please try again later")
+            return 1
+        
+        print("\nEnter your choice of algorithm:")
+        print("\t1. Uniform Cost Search")
+        print("\t2. A* with the Misplaced Tile Heuristic")
+        print("\t3. A* with the Manhattan Distance Heuristic")
+        try:
+            algo_choice = int(input(""))
+        except:
+            print("That doesnt look like a number :(")
+            return 1
+
+        problem = [[row_1], [row_2], [row_3]]
+        print(problem)
+    else:
+        print("Sorry, that was not an option :(")
+        return 1
+
+main()
