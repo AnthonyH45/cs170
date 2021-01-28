a8d4c7f3921dfdb60f1d025876e430eb4185ee64
Anthony Hallak <ahall012@ucr.edu>
2021-01-27 22:52:16 -0800

    Finished main driver code

--- project1/generic.py
+++ project1/generic.py
@@ -2,7 +2,7 @@
 # function and an inital problem state for the 8 puzzle
 
 class Problem:
-    def __init__(self, arr, type=8, goal_state):
+    def __init__(self, arr=[], type=8, goal_state=[]):
         # initial state is the given state from the user
         self.initial_state = arr
         # self.operators = ['left','down','right','up']
@@ -19,17 +19,25 @@ class Node:
 
 def make_node(given_state):
     # TODO
+    return 0
 
 def make_queue(node_arr):
     # TODO
+    return 0
 
-def queueing_function(nodes, expansions):
+# option
+# 1 = Uniform Cost (A* with h(n) == 0)
+# 2 = A* with Misplaced Tile h(n)
+# 3 = A* with Manhattan Distance h(n)
+def queueing_function(option, nodes, expansions):
     # TODO
+    return 0
 
 def expand(curr_node, operators):
     # TODO 
+    return 0
 
-def generic(problem: Problem, queueing_function):
+def generic(problem: Problem, option_choice):
     # we need to make a list of nodes that have an order to visit
     nodes = make_queue(make_node(problem.initial_state))
     while True:
@@ -44,5 +52,5 @@ def generic(problem: Problem, queueing_function):
             return curr_node
         # the node is not the goal state
         # so we expand our nodes and restart
-        nodes = queueing_function(nodes, expand(curr_node, problem.operators))
+        nodes = queueing_function(option_choice, nodes, expand(curr_node, problem.operators))
     return "No solution"
--- project1/main.py
+++ project1/main.py
@@ -6,6 +6,7 @@
  and we get back our solution (if there is one)
 '''
 import random
+from generic import *
 
 def main():
     print("Welcome to Anthony Hallak's 8-puzzle solver!\n")
@@ -22,6 +23,7 @@ def main():
         random.shuffle(random_arr[2])
         random.shuffle(random_arr)
         print(random_arr)
+        problem = random_arr
     elif puzzle_type == 2:
         print("Enter your puzzle, use a zero to represent the blank")
         row_1 = str(input("Enter the first row, use spaces or tabs between numbers\n"))
@@ -39,21 +41,25 @@ def main():
         if len(row_3) < 3 or len(row_3) > 3:
             print("That doesn't look like 3 numbers, please try again later")
             return 1
-        
-        print("\nEnter your choice of algorithm:")
-        print("\t1. Uniform Cost Search")
-        print("\t2. A* with the Misplaced Tile Heuristic")
-        print("\t3. A* with the Manhattan Distance Heuristic")
-        try:
-            algo_choice = int(input(""))
-        except:
-            print("That doesnt look like a number :(")
-            return 1
-
         problem = [[row_1], [row_2], [row_3]]
-        print(problem)
     else:
-        print("Sorry, that was not an option :(")
+        print("That was not understood, sorry")
+
+    print("\nEnter your choice of algorithm:")
+    print("\t1. Uniform Cost Search")
+    print("\t2. A* with the Misplaced Tile Heuristic")
+    print("\t3. A* with the Manhattan Distance Heuristic")
+    try:
+        algo_choice = int(input(""))
+    except:
+        print("That doesnt look like a number :(")
         return 1
+
+    if algo_choice > 3 or algo_choice < 1:
+        print("That doesn't look like a valid option, sorry")
+        return 1
+
+    print(problem)
+    generic(Problem(problem), algo_choice)
 
 main()
