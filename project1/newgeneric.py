8b4a572ded78272f56e0923a419aa8837ea6046b
Anthony Hallak <ahall012@ucr.edu>
2021-02-17 12:34:56 -0800

    Solves Everything

--- project1/newgeneric.py
+++ project1/newgeneric.py
@@ -13,14 +13,14 @@ Node:
 '''
 class Node:
     def __init__(self, state: List[List[int]]=[[1,2,3],[4,5,6],[7,8,0]], depth: int = 0, score: int = 0):
-        self.state = state
-        self.depth = depth
-        self.score = score
-        self.operators = [(-1,0),(0,-1),(1,0),(0,1)] # left, down, right, up
+        self.state = state  # 3x3 grid
+        self.depth = depth  # g(n)
+        self.score = score  # f(n) = g(n) + h(n)
+        self.operators = [(-1,0),(0,-1),(1,0),(0,1)] # (x,y) : left, down, right, up
 
     def find_blank(self):
-        for i in range(0,3):
-            for j in range(0,3):
+        for i in range(0,len(self.state)):
+            for j in range(0,len(self.state[0])):
                 if self.state[i][j] == 0:
                     return (i,j)
 
@@ -31,10 +31,22 @@ class Node:
         print()
 
     # https://docs.python.org/3/reference/datamodel.html#object.__eq__
-    # https://docs.python.org/3/reference/datamodel.html#object.__hash__
     # https://stackoverflow.com/questions/4169252/remove-duplicates-in-list-of-object-with-python/4173307
     def __eq__(self, other):
         return self.state == other.state
+
+    # https://docs.python.org/3/reference/datamodel.html#object.__hash__
+    # https://stackoverflow.com/questions/626759/whats-the-difference-between-lists-and-tuples
+    # https://stackoverflow.com/questions/11142397/does-python-have-an-immutable-list
+    # https://stackoverflow.com/questions/19371358/python-typeerror-unhashable-type-list/19371472
+    # we need an immutable type for hashing in a set
+    # so we return a serialized state as a tuple
+    def __hash__(self):
+        return hash( 
+                    (self.state[0][0],self.state[0][1],self.state[0][2],
+                     self.state[1][0],self.state[1][1],self.state[1][2],
+                     self.state[2][0],self.state[2][1],self.state[2][2])
+                    )
 
 '''
 Problem:
@@ -51,8 +63,8 @@ class Problem:
     def __init__(self, arr: List[List[int]] = [[1,2,3],[4,5,6],[7,8,0]], puzzle_type: int = 8, goal_state: List[List[int]] = [[1,2,3],[4,5,6],[7,8,0]], algo_choice: int = 2):
         self.init_state = Node(arr,0,0)     # inital state is a node with 0 depth and 0 score
         self.puzzle_type = puzzle_type      # determines which puzzle to sovle, right now, only 8 puzzle
-        self.seen: List[List[int]] = []     # to remove duplicates from being seen more than once
-        self.seen_exp = []                  # prevents returning already seen expansions
+        self.seen_set = set()               # use set instead to have O(1) checks for duplicates
+        self.exp_set = set()
         self.algo_choice = algo_choice      # determines which heuristic to use
         self.num_exp = 0                    # keeps track of the number of expansions
         if self.puzzle_type == 8:
@@ -66,19 +78,11 @@ class Problem:
     def make_queue(self, node: Node):
         return [node]
 
-    # https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
-    def rem_dups(self, nodes: List[Node]):
-        to_ret = []
-        for i in nodes:
-            if i not in to_ret:
-                to_ret.append(i)
-        return to_ret
-
     def expand(self, node: Node, operators):
         print("Expanding")
         to_ret = []
         pos_0 = node.find_blank()
-        self.seen_exp.append(node.state)
+        self.exp_set.add(node)
         for i in operators:
             if (pos_0[0]+i[0]) > -1 and (pos_0[0]+i[0]) < 3:
                 if (pos_0[1]+i[1]) > -1 and (pos_0[1]+i[1]) < 3:
@@ -87,14 +91,15 @@ class Problem:
                     to_add_node.state[pos_0[0]+i[0]][pos_0[1]+i[1]] = 0
                     to_add_node.state[pos_0[0]][pos_0[1]] = temp
 
-                    # if to_add_node.state in self.seen_exp:
-                    #     print("already seen this expansion, not adding it to queue")
-                    # else:
-                    #     to_add_node.print()
-                    #     to_ret.append(to_add_node)
-                    #     self.seen_exp.append(to_add_node.state)
-                    # deal with duplicates in queueing
-                    to_ret.append(to_add_node)
+                    # deal with duplicates in queueing ?
+                    # we have less nodes expanded but takes longer
+                    if to_add_node in self.exp_set:
+                        print("already seen this expansion, not adding it to queue")
+                    else:
+                        # to_add_node.print()
+                        to_ret.append(to_add_node)
+                        self.exp_set.add(to_add_node)
+                    # to_ret.append(to_add_node)
 
         if len(to_ret) == 0:
             return []
@@ -109,10 +114,12 @@ class Problem:
             # since everything costs the same, we just add the 
             # expansions to the nodes and return
             for i in expansions:
-                if i.state not in self.seen:
+                if i not in self.seen_set:
                     nodes.append(i)
-                    self.seen.append(i.state)
-            #return self.rem_dups(nodes)
+                    self.seen_set.add(i)
+                
+            print("Best node with g(n)=", nodes[0].depth, "and h(n)=",nodes[0].score - nodes[0].depth)
+            nodes[0].print()
             return nodes
 
         if self.algo_choice == 2:
@@ -120,7 +127,7 @@ class Problem:
 
             to_ret = []
             for exp in expansions:
-                if exp.state not in self.seen:
+                if exp not in self.seen_set: # if we have not seen this node, lets look at it
                     if exp.state == self.goal_state.state:
                         print("Found goal state in queueing, returning!")
                         return [exp]
@@ -139,7 +146,7 @@ class Problem:
                     # so we just add h(n) to g(n) and be done!
                     exp.score += misplaced_sum
                     to_ret.append(exp)
-                    self.seen.append(exp.state)
+                    self.seen_set.add(exp)
 
             # sort expansions by misplaced tiles, smallest first
             # used this https://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
@@ -165,7 +172,7 @@ class Problem:
                     # a = 1
                 # else:
                 # if True:
-                if exp.state not in self.seen:
+                if exp not in self.seen_set:
                     manhattan_d = 0
                     # for each square in the state, we find how far from its goal state square it is
                     # each number's distance is summed and that is assgined as the nodes score  
@@ -186,13 +193,16 @@ class Problem:
                     # so we just add h(n) to g(n) and be done!
                     exp.score += manhattan_d
                     to_ret.append(exp)
-                    self.seen.append(exp.state)
+                    self.seen_set.add(exp)
 
             # sort list of nodes by score
             # used this https://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
             to_ret.sort(key=lambda x: x.score)
+            # nodes.sort(key=lambda x: x.score)
             for i in to_ret:
                 nodes.append(i)
+            # for i in nodes:
+                # to_ret.append(i)
             # nodes.sort(key=lambda x: x.score)
             print("Best node with g(n)=", nodes[0].depth, "and h(n)=",nodes[0].score - nodes[0].depth)
             nodes[0].print()
@@ -223,7 +233,7 @@ def generic(problem: Problem, algo_choice=2):
         max_num_nodes = max( [ len(nodes),max_num_nodes ] )
         curr_node = nodes[0]
         nodes = nodes[1:]
-        problem.seen.append(curr_node.state)
+        problem.seen_set.add(curr_node)
 
         # print("Current Depth:",curr_node.depth)
         # print("Looking at")
@@ -233,8 +243,8 @@ def generic(problem: Problem, algo_choice=2):
             end = time.time()
             problem.print_algo()
             print("Found solution at depth", curr_node.depth)
-            print("Total number of expanded nodes:", problem.num_exp)
-            print("Total number of seen nodes:",len(problem.seen))
+            print("Total number of expanded nodes:", len(problem.exp_set))#problem.num_exp)
+            print("Total number of seen nodes:",len(problem.seen_set))
             print("Largest queue size:",max_num_nodes)
             print("Seconds elapsed WITHOUT printing:", end-start)
             return curr_node.state
--- project1/tester.py
+++ project1/tester.py
@@ -20,17 +20,20 @@ we can then look at the results later
 '''
 # =================================================
 from newgeneric import *
+import time
+# list of tuples with ( depth, problem )
+# all problems were given from prof
 problems = [
-    [[1,2,3],[4,5,6],[7,8,0]], # depth 0
-    [[1,2,3],[4,5,6],[0,7,8]], # depth 2
-    [[1,2,3],[5,0,6],[4,7,8]], # depth 4
-    [[1,3,6],[5,0,2],[4,7,8]], # depth 8
-    [[1,3,6],[5,0,7],[4,8,2]], # depth 12
-    [[1,6,7],[5,0,3],[4,8,2]], # depth 16
-    [[7,1,2],[4,8,5],[6,3,0]], # depth 20
-    # [[0,7,2],[4,6,1],[3,5,8]], # depth 24
-    # [[8,6,7],[2,5,4],[3,0,1]], # depth 31
-    # [[6,4,7],[8,5,0],[3,2,1]], # depth 31
+    (0, [[1,2,3],[4,5,6],[7,8,0]]), # depth 0
+    (2, [[1,2,3],[4,5,6],[0,7,8]]), # depth 2
+    (4, [[1,2,3],[5,0,6],[4,7,8]]), # depth 4
+    (8, [[1,3,6],[5,0,2],[4,7,8]]), # depth 8
+    (12, [[1,3,6],[5,0,7],[4,8,2]]), # depth 12
+    (16, [[1,6,7],[5,0,3],[4,8,2]]), # depth 16
+    (20, [[7,1,2],[4,8,5],[6,3,0]]), # depth 20
+    (24, [[0,7,2],[4,6,1],[3,5,8]]), # depth 24
+    (31, [[8,6,7],[2,5,4],[3,0,1]]), # depth 31
+    (31, [[6,4,7],[8,5,0],[3,2,1]]), # depth 31
 ]
 algo_choice = [1,2,3]
 
@@ -38,8 +41,9 @@ for p in problems:
     print("=================================================")
     for a in algo_choice:
         print("START")
-        print("Solution?",generic(Problem(p,8,0,a)))
-        print("Original state",p)
+        print("Solution?",generic(Problem(p[1],8,0,a)))
+        print("Original state",p[1])
         print("END")
         print()
+        time.sleep(1)
     print("=================================================")
