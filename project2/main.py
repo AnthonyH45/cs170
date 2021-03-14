2ad03d8010a1e563c34feb347c944a89ccd39e87
Anthony Hallak <ahall012@ucr.edu>
2021-03-14 13:50:24 -0700

    ignore this, moving code to desktop

--- project2/main.py
+++ project2/main.py
@@ -1,22 +1,53 @@
 from io import TextIOWrapper
+import math
+import time
 from typing import *
 
-def validate(data, j):
-    return j[0]
+# https://stackoverflow.com/questions/4362586/sum-a-list-of-numbers-in-python
+# takes in data, j (current row), (r,c) location of j in data
+def validate(data: List[List[float]], curr: List[float], r: int): #, c: int):
+    num_correct = 0
+    nns = []
+    for i,j in enumerate(data):
+        # print("finding nn for",j)
+        nn_dist = float('INF')
+        nn_loc = float('INF')
+        nn_class = float(0)
+        if i != r:
+            dist = math.sqrt(sum([(a-b)**2 for (a,b) in zip(j[1:],curr)]))
+            if dist < nn_dist:
+                nn_dist = dist
+                nn_loc = i
+                nn_class = j[0]
+                nns.append(j)
+
+    time.sleep(1)
+    print(nns)
+    time.sleep(1)
+    for n in nns:
+        if n[0] == curr[0]:
+            num_correct += 1
+
+    acc = (num_correct / len(data))
+    print("Accuracy:",acc)
+    return acc
+    
 
 def fs(data: List[List[float]]):
+    # set of tuples (category, feature)
+    to_add_level = set() 
     for i,j in enumerate(data):
         print("On level:",i)
-        to_add_level = [] # make set so we dont add
         best_accuracy = 0
-        for i in range(1,len(j)):
-            print("--Considering adding the",i,"feature")
-            current_accuracy = validate(data,j[1:])
-            print("val:",j)
-            if best_accuracy < current_accuracy:
-                best_accuracy = current_accuracy
-                to_add_level.append(j)
-        print("Best feature to add on level",i,"is feature",to_add_level[-1])
+        for k in range(1,len(j)):
+            if (i,k) not in to_add_level:
+                print("--Considering adding the",k,"feature")
+                current_accuracy = validate(data,j[1:], i) #, k)
+                time.sleep(1)
+                print("val:",j)
+                if best_accuracy < current_accuracy:
+                    best_accuracy = current_accuracy
+                    to_add_level.add((i,k))
 
 # https://stackoverflow.com/questions/6492096/automatically-process-numbers-in-e-scientific-notation-in-python
 # https://stackoverflow.com/questions/44461551/how-to-print-map-object-with-python-3
@@ -25,8 +56,9 @@ def fs(data: List[List[float]]):
 # https://docs.python.org/3/library/io.html#io.TextIOBase
 # CS170_largetestdata__9.txt
 # CS170_SMALLtestdata__72.txt
-
 # reads the given TextIOWrapper 
+# parses the txt file and converts the 
+# numbers to proper floats, rather than scientific number
 def parse(f: TextIOWrapper):
     return [[float(j) for j in i.split()] for i in f.readlines()]
 
@@ -54,4 +86,5 @@ def main():
         print("Using Backward Elimination")
         be(parsed_data)
 
-main()
+if __name__ == '__main__':
+    main()
