8e607ad3745d3aae56699223c40330c4ba6dfd1f
Anthony Hallak <ahall012@ucr.edu>
2021-03-17 16:08:01 -0700

    rust version can read and parse, still needs A LOT of work

--- project2/main/src/main.rs
+++ project2/main/src/main.rs
@@ -8,32 +8,72 @@
 // https://doc.rust-lang.org/std/env/index.html
 // https://doc.rust-lang.org/std/path/struct.PathBuf.html
 // https://github.com/PascalPrecht/rustlings
+// https://stackoverflow.com/questions/26643688/how-do-i-split-a-string-in-rust
+// https://doc.rust-lang.org/std/string/struct.String.html
+// https://doc.rust-lang.org/std/convert/trait.Into.html
+// https://stackoverflow.com/questions/31497422/filter-all-non-integers-from-string-and-yield-vector
+// https://doc.rust-lang.org/std/primitive.char.html
+// https://doc.rust-lang.org/std/string/struct.String.html#method.chars
+// https://stackoverflow.com/questions/45282970/does-rust-have-an-equivalent-to-pythons-list-comprehension-syntax/45283083
+// https://doc.rust-lang.org/std/vec/struct.Vec.html
+// https://doc.rust-lang.org/1.2.0/book/for-loops.html
+// https://mkaz.blog/working-with-rust/numbers/
+// https://doc.rust-lang.org/std/fs/struct.OpenOptions.html
+// https://stackoverflow.com/questions/65862510/why-am-i-getting-a-bad-file-descriptor-error-when-writing-file-in-rust
+// https://doc.rust-lang.org/std/fs/struct.File.html
+// https://doc.rust-lang.org/1.2.0/book/for-loops.html
 // # CS170_largetestdata__9.txt
 // # CS170_SMALLtestdata__72.txt
 
-use std::{env, fs::File, io, io::prelude::*};
+use std::{fs::File, io::prelude::*};
 
-fn main() -> std::io::Result<()> {
-    println!("Welcome to Anthony Hallak's Feature Selection Algorithm!");
-    println!("Type in the name of the file to test: ");
-    let mut filename = String::new();
-    // let filename = "/home/zax/repos/cs170/project2/CS170_SMALLtestdata__72.txt";
-    io::stdin().read_line(&mut filename).expect("Cannot read input");
-
-    let mut curr_dir = env::current_dir()?;
-    curr_dir.push(filename);
-    let mut unwrap = curr_dir.into_os_string().into_string().unwrap();
-    println!("{}",unwrap);
-    let mut file = File::open(&mut unwrap)?;
-    println!("21got here");
+fn read_data() -> String {
+    let filename = "/mnt/c/Users/zax45/codeSpace/cs170/project2/main/CS170_SMALLtestdata__72.txt";
+    // std::io::stdin().read_line(&mut filename).expect("Cannot read input");
+    let mut file = File::open(filename).expect("Cannot open file");
 
     let mut contents = String::new();
-    println!("24got here");
-    file.read_to_string(&mut contents)?;
+    file.read_to_string(&mut contents).expect("Cannot read file");
 
-    println!("{}",contents);
-    Ok(())
+    let to_parse = contents.split_whitespace().collect::<Vec<&str>>();
+    // let mut to_return = Vec::new();
+    // 300 rows
+    for (i,j) in to_parse.iter().enumerate() {
+        println!("i = {} and j = {}", i, j.parse::<f64>().unwrap());
+    }
+    // println!("{:?}",to_parse);
+
+    // small has 11 columns, 300 rows
+    // let mut data = Vec::new();
+    // println!("{:?}",contents.chars());
+
+    return contents;
 }
+
+// fn main() -> std::io::Result<()> {
+fn main() {
+    println!("Welcome to Anthony Hallak's Feature Selection Algorithm!");
+    let data = read_data();
+    // println!("{}",data);
+}
+
+// println!("Type in the name of the file to test: ");
+// let mut filename = String::new();
+// let filename = "/home/zax/repos/cs170/project2/CS170_SMALLtestdata__72.txt";
+    // let mut curr_dir = env::current_dir()?;
+    // curr_dir.push(filename);
+    // let mut unwrap = curr_dir.into_os_string().into_string().unwrap();
+    // println!("{}",unwrap);
+    // let mut file = File::open(&mut unwrap)?;
+    // println!("21got here");
+
+    // let mut contents = String::new();
+    // println!("24got here");
+    // file.read_to_string(&mut contents)?;
+
+    // println!("{}",contents);
+    // Ok(())
+// }
 
 //     // let contents = fs::read_to_string(filename)
 //     //     .expect("Something went wrong reading the file");
